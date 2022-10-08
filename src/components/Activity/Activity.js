c81e1cfee20595235f786d5109f39fc34348aad1
Rocksana Akter <rocksanaomi1176@gmail.com>
2022-10-08 07:04:27 +0600

    api-json-fakedata

---
+++ src/components/Activity/Activity.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Activity.css'
+const Activity = () => {
+    return (
+        <div className='activity-container'>
+            <div className="products-container">product list</div>
+            <div className="cart-container">calculation</div>
+        </div>
+    );
+};
+
+export default Activity;
