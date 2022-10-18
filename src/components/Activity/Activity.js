10d7f3d7e76fd4cb25b2dd3250988426320ad832
Rocksana Akter <rocksanaomi1176@gmail.com>
2022-10-18 15:07:41 +0600

    addBtn

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -3,21 +3,40 @@ import './Activity.css'
 import Product from '../Product/Product.js'
 const Activity = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+    
     useEffect(()=>{
         fetch('products.json')
         .then(res=>res.json()
         .then(data=>setProducts(data)))
     },
     []);
+
+    const handleAddToCart = (product) =>{
+        console.log(product)
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='activity-container'>
             <div className="products-container">
                 {
                     products.map(product=> <Product key={product.id}
-                    product={product}></Product>)
+                    product={product}
+                    handleAddToCart={handleAddToCart}></Product>)
                 }
             </div>
-            <div className="cart-container">calculation</div>
+            <div className="cart-container">
+                <h2>Activity Details</h2>
+                <div>
+                    <div>
+<p>Challenge time: {cart.length}</p>
+                    </div>
+                    <div>
+<p>Break Time:</p>
+                    </div>
+                </div>
+                calculation</div>
         </div>
     );
 };
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,13 +1,20 @@
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
+    
     const {image, title, duration} = props.product;
     return (
         <div className='product'>
             <img src ={image} alt=""></img>
+            <div className='product-info'>
             <p className='product-title'>Tittle: {title}</p >
             <p>Time Required: {duration}</p>
+            </div>
+            <button onClick={()=>props.handleAddToCart(props.product)} className='btn-cart'><p>Add To List</p>
+            <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon></button>
         </div>
     );
 };
